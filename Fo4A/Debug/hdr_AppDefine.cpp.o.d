8cce3ab57cc4fbd9f12714052b5419cb7aef4119
FrancisLD <laidanghungptit@gmail.com>
2018-07-07 18:20:59 +0700

    [FL][Remove] Remove Debug folder

--- Fo4A/Debug/hdr_AppDefine.cpp.o.d
+++
@@ -1,3 +0,0 @@
-Debug/hdr_AppDefine.cpp.o: hdr/AppDefine.cpp hdr/AppDefine.h
-
-hdr/AppDefine.h:
--- Fo4A/Debug/hdr_AppManagement.cpp.o.d
+++
@@ -1,3 +0,0 @@
-Debug/hdr_AppManagement.cpp.o: hdr/AppManagement.cpp hdr/AppManagement.h
-
-hdr/AppManagement.h:
